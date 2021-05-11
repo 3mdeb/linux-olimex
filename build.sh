7d6fbfcde323e5137f22566f143db25e5bfcd8e9
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2021-05-11 15:09:35 +0200

    build.sh: extend script
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,15 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
-make olinuxino_defconfig
-make -j$(nproc) bindeb-pkg LOCALVERSION=-olimex KDEB_PKGVERSION=$(make kernelversion)-$(date +%Y%m%d-%H%M%S) DTC_FLAGS=-@
+# build DTB overlays
+if [ ! -d olinuxino-overlays ]; then
+  git clone https://github.com/OLIMEX/olinuxino-overlays.git
+  cd olinuxino-overlays
+  make clean
+  make
+  cd ..
+fi
+
+make olinuxino_micro_defconfig
+make -j$(nproc) LOCALVERSION=-olimex KDEB_PKGVERSION=$(make kernelversion)-$(date +%Y%m%d-%H%M%S) DTC_FLAGS=-@
+mkimage -f kernel.its kernel.itb
